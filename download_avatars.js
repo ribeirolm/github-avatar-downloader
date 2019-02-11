a0e82b6a614a2be0f031444e4c0cf6675defb8b3
Lauren <laurenribeiro06@gmail.com>
2019-02-11 23:46:28 +0000

    Added functionality to now print a list of avatar urls for the users

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,9 @@
 // In this activity, you will partially implement a single function called getRepoContributors. Using the request package, you will fetch the list of contributors as a JSON string from the GitHub API's contributors endpoint. Upon receiving the results, your function will invoke a callback function with the results. This callback function will loop over and print out the avatar_url for each object in the collection.
 
 
-//In the next step you will implement the function to use the request library to programmatically fetch the list of contributors via HTTPS for the given repo.
+//Your next and final step in this exercise should be to change your getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
+
+//You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
 
 var secrets = require('./secrets.js');
 var request = require('request');
@@ -18,11 +20,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+  request(options, function (err, res, body) {
+    if (!err && res.statusCode === 200) {
+    var info = JSON.parse(body);
+    info.forEach(function(user){
+      var avatar = user.avatar_url;
+      console.log(avatar);
+    })
+    }
   });
 }
-
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
