94a41fe098e338eb01bc711ec56bdf9a5965b28f
Lauren <laurenribeiro06@gmail.com>
2019-02-12 01:40:19 +0000

    Added support for command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,32 +1,32 @@
-// In this activity, you will partially implement a single function called getRepoContributors. Using the request package, you will fetch the list of contributors as a JSON string from the GitHub API's contributors endpoint. Upon receiving the results, your function will invoke a callback function with the results. This callback function will loop over and print out the avatar_url for each object in the collection.
-
-
-//Your next and final step in this exercise should be to change your getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
-
-//You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
-
 var secrets = require('./secrets.js');
 var request = require('request');
 var https = require('https');
 var fs = require('fs');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `token ${secrets.GITHUB_TOKEN}`
-    }
-  };
-
-  request(options, function (err, res, body) {
-    if (!err && res.statusCode === 200) {
-    var info = JSON.parse(body);
-    cb(err, info);
+  if (repoOwner === undefined || repoName === undefined) {
+    console.log("This application cannot run without a specified repo owner and repo name in the command line!");
+  } else {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': `token ${secrets.GITHUB_TOKEN}`
+      }
     };
-  });
+    // console.log(options.url);
+
+    request(options, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+      var info = JSON.parse(body);
+      cb(err, info);
+      };
+    });
+  }
 }
 
 function downloadImageByURL(avatar, filePath) {
@@ -40,12 +40,19 @@ function downloadImageByURL(avatar, filePath) {
   .pipe(fs.createWriteStream(`./avatars/${filePath}.jpg`));
 }
 
-
-
-getRepoContributors("jquery", "jquery", function(err, info){
+getRepoContributors(process.argv[2], process.argv[3], function(err, info){
   info.forEach(function(user){
       var avatar = user.avatar_url;
       var filePath = user.login;
       downloadImageByURL (avatar, filePath);
   });
 });
+
+
+// getRepoContributors("jquery", "jquery", function(err, info){
+//   info.forEach(function(user){
+//       var avatar = user.avatar_url;
+//       var filePath = user.login;
+//       downloadImageByURL (avatar, filePath);
+//   });
+// });
